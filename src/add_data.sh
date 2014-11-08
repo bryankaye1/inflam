f74b297f622fdb4050100349fae89c8887a2f4f4
Bryan Kaye <bryankaye1@gmail.com>
2014-11-08 16:21:05 -0500

    big commit

--- src/add_data.sh
+++ src/add_data.sh
@@ -3,4 +3,11 @@ filename=$2
 
 drugname=`python assign_drug.py $filename`
 
-echo cp data/$filename/$date/${date}-${drugname}.dat
+fpn=~/Desktop/python/${filename}
+dest=~/Desktop/inflam/data/${date}-${drugname}-${filename}
+cp $fpn $dest
+#echo git add -$dest
+
+#echo msg="added $filename"
+#echo git commit -m "'$msg'"
+#echo ="new file added to repo '$msg'"
